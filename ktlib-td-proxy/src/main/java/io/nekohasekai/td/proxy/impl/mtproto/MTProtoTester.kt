73fe1224e38e32ee8227ea4fc11ad29a3c980dcf
世界 <sekai@neko.services>
2021-04-02 09:40:59 +0800

    Fix tester

--- ktlib-td-proxy/src/main/java/io/nekohasekai/td/proxy/impl/mtproto/MTProtoTester.kt
+++ ktlib-td-proxy/src/main/java/io/nekohasekai/td/proxy/impl/mtproto/MTProtoTester.kt
@@ -8,19 +8,36 @@ import io.nekohasekai.ktlib.td.core.raw.addProxy
 import io.nekohasekai.ktlib.td.core.raw.pingProxyWith
 import io.nekohasekai.td.proxy.tester.ProxyTester
 import io.nekohasekai.td.proxy.tester.testTimeout
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeoutOrNull
 import td.TdApi
+import java.net.InetSocketAddress
+import java.net.Socket
 
 object MTProtoTester : TdHandler(), ProxyTester<MTProtoProxy> {
 
     override suspend fun testProxy(proxy: MTProtoProxy): Int {
 
+        withContext(Dispatchers.IO) {
+
+            try {
+                val socket = Socket()
+                socket.connect(InetSocketAddress(proxy.server, proxy.port), (15000))
+                socket.close()
+            } catch (e: Exception) {
+                throw TdException(-1, e.javaClass.simpleName)
+            }
+
+        }
+
         var result = -1
         var exception: Exception? = null
         val finishLock = Mutex(true)
+        val pid = addProxy(proxy.server, proxy.port, false, TdApi.ProxyTypeMtproto(proxy.secret)).id
 
-        pingProxyWith(addProxy(proxy.server, proxy.port, false, TdApi.ProxyTypeMtproto(proxy.secret)).id) {
+        pingProxyWith(pid) {
 
             onSuccess {
 
@@ -43,6 +60,8 @@ object MTProtoTester : TdHandler(), ProxyTester<MTProtoProxy> {
             finishLock.lock()
 
         }
+
+        sendRaw(TdApi.RemoveProxy(pid))
 
         if (result != -1) return result
 
